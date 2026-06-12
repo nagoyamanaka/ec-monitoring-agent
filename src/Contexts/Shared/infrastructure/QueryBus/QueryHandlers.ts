30a164838d0d9a66c39bfb1394b2cd5c01b06dfe
shigeyasushimamura <sgys.mtk@gmail.com>
2026-06-12 16:54:31 +0900

    不要な記載削除

--- src/Contexts/Shared/infrastructure/QueryBus/QueryHandlers.ts
+++ src/Contexts/Shared/infrastructure/QueryBus/QueryHandlers.ts
@@ -1,18 +1,18 @@
-import { Query } from '../../domain/Query.js';
-import { QueryHandler } from '../../domain/QueryHandler.js';
-import { Response } from '../../domain/Response.js';
-import { QueryNotRegisteredError } from '../../application/errors/QueryNotRegisteredError.js';
+import { Query } from "../../domain/Query.js";
+import { QueryHandler } from "../../domain/QueryHandler.js";
+import { Response } from "../../domain/Response.js";
+import { QueryNotRegisteredError } from "../../application/errors/QueryNotRegisteredError.js";
 
 export class QueryHandlers extends Map<Query, QueryHandler<Query, Response>> {
   constructor(queryHandlers: Array<QueryHandler<Query, Response>>) {
     super();
-    queryHandlers.forEach(queryHandler => {
+    queryHandlers.forEach((queryHandler) => {
       this.set(queryHandler.subscribedTo(), queryHandler);
     });
   }
 
   public get(query: Query): QueryHandler<Query, Response> {
-    const queryHandler = super.get(query.constructor as unknown as Query);
+    const queryHandler = super.get(query.constructor);
 
     if (!queryHandler) {
       throw new QueryNotRegisteredError(query);
